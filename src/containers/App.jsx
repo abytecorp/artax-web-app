5616f9b05d547db1ec398aac0e3df629438f21f8
Bryan Murcia <bryanmurcia@MacBook-Air-de-Bryan.local>
2019-12-15 07:32:36 -0500

    adding css

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
-// import '../assets/styles/scss/style.scss';
+import '../assets/styles/App.scss';
 
 const App = () => {
   return (
-    <Header />
+    <div className='grid-container'>
+      <Header />
+      <aside className='sidenav' />
+      <main className='main' />
+      <footer className='footer' />
+    </div>
   );
 };
 
 export default App;
-// https://templatemag.com/dashio-bootstrap-admin-template/ 
